075eeaa3a5327e4fe9e6f60124e960c12559a3c6
Jaelhp <69092636+Jaelhp@users.noreply.github.com>
2024-06-02 13:03:43 -0500

    se agrego comentarios a nuestro codigo

--- DeteccionPlagio.java
+++ DeteccionPlagio.java
@@ -57,7 +57,7 @@ public class DeteccionPlagio extends JFrame {
             resultTextArea.setText("Error al comprobar el plagio.");
         }
     }
-
+//nuestro main
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
